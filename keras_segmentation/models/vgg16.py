222287ab06891504e4b970129e79f8ef3f7aa524
guptapre <104062327+guptapre@users.noreply.github.com>
2023-01-16 17:05:15 +0100

    Update vgg16.py

--- keras_segmentation/models/vgg16.py
+++ keras_segmentation/models/vgg16.py
@@ -80,8 +80,11 @@ def get_vgg_encoder(input_height=224,  input_width=224, pretrained='imagenet', c
         VGG_Weights_path = tf.keras.utils.get_file(
             pretrained_url.split("/")[-1], pretrained_url)
         Model(img_input, x).load_weights(VGG_Weights_path, by_name=True, skip_mismatch=True)
-        
+        print("2")
+    print("3")
+    
     if pretrained == 'MaSTr1325_Unet':
+        print("1")
         VGG_Weights_path = tf.keras.utils.get_file(
             pretrained_url_MaSTr1325_Unet.split("/")[-1], pretrained_url_MaSTr1325_Unet)
         Model(img_input, x).load_weights(VGG_Weights_path, by_name=True, skip_mismatch=True)
